c6a43e01dcd4e9ac3b742ed060b708dfeabed72e
wangjian <wangjian0504@gmail.com>
2024-10-09 17:45:12 +0800

    chore: replay

--- src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod opts;
-mod process;
-
-pub use opts::{Opts, SubCommand};
-pub use process::{process_csv, process_genpass};
--- src/process.rs
+++
@@ -1,5 +0,0 @@
-mod csv_convert;
-mod gen_pass;
-
-pub use csv_convert::process_csv;
-pub use gen_pass::process_genpass;
