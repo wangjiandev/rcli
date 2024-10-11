aa208715ec6f7d7e62daee311b1f3fd99d29aa4a
wangjian <wangjian0504@gmail.com>
2024-10-11 15:56:39 +0800

    feat: add gen password Features

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,18 @@ use rcli::{
 };
 
 /// rcli csv -i input.csv -o output.json --header -d ','
+/// rcli gen-pass --length 16 --uppercase --lowercase --numbers --symbols
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Csv(options) => process::process_csv(
-            &options.input,
-            &options.output,
-            options.header,
-            options.delimiter,
-        )?,
+        Commands::Csv(options) => {
+            println!("CsvOptions: {:?}", options);
+            process::process_csv(&options.input, &options.output, &options.format)?
+        }
+        Commands::GenPass(options) => {
+            println!("GenPassOptions: {:?}", options);
+            process::process_genpass(&options)?
+        }
     }
     Ok(())
 }
